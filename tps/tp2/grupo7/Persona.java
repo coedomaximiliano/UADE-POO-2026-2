04080a4483cda2c4071c42e8763adb8744c7d1ec
EbuczekUADE <buczek360@gmail.com>
2026-08-14 19:31:39 -0300

    TP2 Grupo7: agregar uso de objetos

---
+++ tps/tp2/grupo7/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void saludar() {
+        System.out.println("Hola, mi nombre es " + nombre);
+    }
+}
